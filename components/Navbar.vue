b103a2b80bfd6ff3a40a9d260dccf5f5ee53be81
Nanachi <dinhvu2509@gmail.com>
2021-10-19 15:33:51 +0700

    spacing

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,11 +1,13 @@
 <template>
 	<Container>
-		<div class="flex sticky py-3">
-			<NuxtLink to="/" class="font-bold text-3xl mr-2 hover:underline"> Nanachi </NuxtLink>
+		<div class="grid grid-cols-6 sticky py-3">
+			<div class="col-span-1">
+				<NuxtLink to="/" class="font-bold text-3xl mr-2"> Nanachi </NuxtLink>
+			</div>
 
-			<div class="flex items-end ml-10">
-				<NuxtLink to="/about" class="mr-2 hover:underline"> About </NuxtLink>
-				<NuxtLink to="/projects" class="mr-2 hover:underline"> Projects </NuxtLink>
+			<div class="col-auto flex items-end">
+				<NuxtLink to="/about" class="mr-3 hover:underline"> About </NuxtLink>
+				<NuxtLink to="/projects" class="mr-3 hover:underline"> Projects </NuxtLink>
 			</div>
 		</div>
 	</Container>
--- pages/about.vue
+++ pages/about.vue
@@ -3,115 +3,117 @@
 		<Header page="About me"></Header>
 
 		<Container>
-			<div class="px-32">
-				<section class="grid grid-cols-4 gap-4 py-5">
-					<div class="col-span-1">
-						<img class="object-cover rounded-full" src="~/assets/img/avatar.jpeg" />
-					</div>
-
-					<div class="col-span-3 flex">
-						<div class="self-center">
-							<h3 class="text-3xl font-bold">Dinh Vu</h3>
-							<h4>09/25/2000</h4>
-							<h4>Full-stack developer</h4>
+			<div class="grid grid-cols-6">
+				<div class="col-start-2 col-span-4">
+					<section class="grid grid-cols-4 gap-4 py-5">
+						<div class="col-span-1">
+							<img class="object-cover rounded-full" src="~/assets/img/avatar.jpeg" />
 						</div>
-					</div>
-				</section>
 
-				<section class="py-5">
-					<header>
-						<span class="underline text-xl">Education</span> <span class="fas fa-graduation-cap"></span>
-					</header>
-					<div class="grid grid-cols-6 gap-4">
-						<div class="col-span-1">2018</div>
-						<div class="col-span-5">Graduated from Nguyen Hue High School for the Gifted</div>
-					</div>
-					<div class="grid grid-cols-6 gap-4">
-						<div class="col-span-1">Now</div>
-						<div class="col-span-5">
-							Studying Applied mathematics at University of Transport and Communications
+						<div class="col-span-3 flex">
+							<div class="self-center">
+								<h3 class="text-3xl font-bold">Dinh Vu</h3>
+								<h4>09/25/2000</h4>
+								<h4>Full-stack developer</h4>
+							</div>
 						</div>
-					</div>
-				</section>
+					</section>
 
-				<section class="py-5">
-					<header>
-						<span class="underline text-xl">Work</span> <span class="fas fa-laptop-code"></span>
-					</header>
-
-					<div class="grid grid-cols-6 gap-4">
-						<div class="col-span-1">2020-2021</div>
-						<div class="col-span-5">Intern at SlimCRM</div>
-					</div>
-				</section>
-
-				<section class="py-5">
-					<header>
-						<span class="underline text-xl">Facts</span> <span class="far fa-smile-wink"></span>
-					</header>
-
-					<ul class="list-disc list-outside">
-						<li>
-							My nickname (Nanachi) is named after
-							<a class="text-red-500 underline" href="https://madeinabyss.fandom.com/wiki/Nanachi"
-								>a character in manga/anime series Made in Abyss.</a
-							>
-						</li>
-						<li>I enjoy playing League of Legends and osu! in my free time.</li>
-						<li>I also love music.</li>
-						<li>I used to study art but soon figured I didn't have the talent for it :(</li>
-					</ul>
-				</section>
-
-				<section class="py-5">
-					<header>
-						<span class="underline text-xl">Social</span> <span class="fas fa-share-alt"></span>
-					</header>
-
-					<a
-						class="inline-block p-3 transition duration-300 rounded-md hover:bg-red-500"
-						href="https://github.com/nanachi-code"
-						><span class="fab fa-github"></span> nanachi-code</a
-					>
-					<a
-						class="inline-block p-3 transition duration-300 rounded-md hover:bg-red-500"
-						href="https://facebook.com/SmugNanachi"
-						><span class="fab fa-facebook"></span> Đinh Vũ</a
-					>
-					<a
-						class="inline-block p-3 transition duration-300 rounded-md hover:bg-red-500"
-						href="https://www.youtube.com/channel/UCwKAldPUtaSwemUjiXNjOTQ"
-						><span class="fab fa-youtube"></span> Nanachi
-					</a>
-
-					<a
-						class="inline-block p-3 transition duration-300 rounded-md hover:bg-red-500"
-						href="https://osu.ppy.sh/users/10063190">
-						<img class="inline-block osu-logo" src="~/assets/img/osu-logo-invert.png" /> Smug Nanachi
-					</a>
-
-					<button
-						ref="copyDiscord"
-						class="
-							copyDiscord
-							inline-block
-							p-3
-							transition
-							duration-300
-							rounded-md
-							hover:bg-red-500
-							relative
-						"
-						@click="copyDiscordTag"
-						@mouseout="resetTooltip">
-						<span class="fab fa-discord"></span> Nanachi#1381
-
-						<div ref="tooltip" class="tooltip transition duration-300" :class="{ copied: copied }">
-							{{ tooltip }}
-							<div class="arrow"></div>
+					<section class="py-5">
+						<header>
+							<span class="underline text-xl">Education</span> <span class="fas fa-graduation-cap"></span>
+						</header>
+						<div class="grid grid-cols-6 gap-4">
+							<div class="col-span-1">2018</div>
+							<div class="col-span-5">Graduated from Nguyen Hue High School for the Gifted</div>
 						</div>
-					</button>
-				</section>
+						<div class="grid grid-cols-6 gap-4">
+							<div class="col-span-1">Now</div>
+							<div class="col-span-5">
+								Studying Applied mathematics at University of Transport and Communications
+							</div>
+						</div>
+					</section>
+
+					<section class="py-5">
+						<header>
+							<span class="underline text-xl">Work</span> <span class="fas fa-laptop-code"></span>
+						</header>
+
+						<div class="grid grid-cols-6 gap-4">
+							<div class="col-span-1">2020-2021</div>
+							<div class="col-span-5">Intern at SlimCRM</div>
+						</div>
+					</section>
+
+					<section class="py-5">
+						<header>
+							<span class="underline text-xl">Facts</span> <span class="far fa-smile-wink"></span>
+						</header>
+
+						<ul class="list-disc list-outside">
+							<li>
+								My nickname (Nanachi) is named after
+								<a class="text-red-500 underline" href="https://madeinabyss.fandom.com/wiki/Nanachi"
+									>a character in manga/anime series Made in Abyss.</a
+								>
+							</li>
+							<li>I enjoy playing League of Legends and osu! in my free time.</li>
+							<li>I also love music.</li>
+							<li>I used to study art but soon figured I didn't have the talent for it :(</li>
+						</ul>
+					</section>
+
+					<section class="py-5">
+						<header>
+							<span class="underline text-xl">Social</span> <span class="fas fa-share-alt"></span>
+						</header>
+
+						<a
+							class="inline-block p-3 transition duration-300 rounded-md hover:bg-red-500"
+							href="https://github.com/nanachi-code"
+							><span class="fab fa-github"></span> nanachi-code</a
+						>
+						<a
+							class="inline-block p-3 transition duration-300 rounded-md hover:bg-red-500"
+							href="https://facebook.com/SmugNanachi"
+							><span class="fab fa-facebook"></span> Đinh Vũ</a
+						>
+						<a
+							class="inline-block p-3 transition duration-300 rounded-md hover:bg-red-500"
+							href="https://www.youtube.com/channel/UCwKAldPUtaSwemUjiXNjOTQ"
+							><span class="fab fa-youtube"></span> Nanachi
+						</a>
+
+						<a
+							class="inline-block p-3 transition duration-300 rounded-md hover:bg-red-500"
+							href="https://osu.ppy.sh/users/10063190">
+							<img class="inline-block osu-logo" src="~/assets/img/osu-logo-invert.png" /> Smug Nanachi
+						</a>
+
+						<button
+							ref="copyDiscord"
+							class="
+								copyDiscord
+								inline-block
+								p-3
+								transition
+								duration-300
+								rounded-md
+								hover:bg-red-500
+								relative
+							"
+							@click="copyDiscordTag"
+							@mouseout="resetTooltip">
+							<span class="fab fa-discord"></span> Nanachi#1381
+
+							<div ref="tooltip" class="tooltip transition duration-300" :class="{ copied: copied }">
+								{{ tooltip }}
+								<div class="arrow"></div>
+							</div>
+						</button>
+					</section>
+				</div>
 			</div>
 		</Container>
 	</div>
